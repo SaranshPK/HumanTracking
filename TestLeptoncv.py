3db2658e7684108207d0fa537b86aae71e92019f
SaranshPK <saranshis@hotmail.com>
2016-08-25 14:32:01 -0700

    werks!

--- TestLeptoncv.py
+++ TestLeptoncv.py
@@ -24,6 +24,7 @@ class hello:
         return json_data
 
 
+
 if __name__ == "__main__":
     app.run()
 
@@ -35,17 +36,8 @@ class Blob:
 
 
 currentSize = 0
-upCount = 0
-rightCount = 0
-leftCount = 0
-downCount = 0
-xMidSum = 0
-yMidSum = 0
 xSum = 0
 ySum = 0
-r = 0
-g = 0
-b = 0
 blobs = []
 json_data = None
 pixdata = None
@@ -53,58 +45,48 @@ image = None
 
 
 def reset():
-    global upCount
-    global rightCount
-    global leftCount
-    global downCount
-    global xMidSum
-    global yMidSum
     global xSum
     global ySum
     global currentSize
-    upCount = 0
-    rightCount = 0
-    leftCount = 0
-    downCount = 0
-    xMidSum = 0
-    yMidSum = 0
     xSum = 0
     ySum = 0
     currentSize = 0
 
 
 def analyze():
-    global upCount, rightCount, leftCount, downCount, xMidSum, yMidSum, xSum, ySum, currentSize, r, g, b, image
+    global xSum, ySum, currentSize, r, g, b, image
     blobCount = 0
-    tempBlobs = blobs
+    tempBlobs = []
+    for blob in blobs:
+        tempBlobs.append(blob)
     for y in xrange(image.size[1]):
         for x in xrange(image.size[0]):
-            if (checkValid(x,y)):
+            if checkValid(x, y):
                 reset()
-                r = randint(0, 100)
-                g = randint(0, 100)
-                b = randint(0, 100)
-                currentSize+=1
-                fillAll(x,y)
+                ySum += y
+                xSum += x
+                currentSize += 1
+                pixdata[x, y] = (0, 0, 0)
+                fillAll(x, y)
                 if currentSize > 10:
                     if xSum == 0:
                         xMid = x
                     else:
-                        xMid = xMidSum /xSum + x
+                        xMid = xSum / float(currentSize)
 
                     if ySum == 0:
                         yMid = y
                     else:
-                        yMid = yMidSum /ySum + y
-                    midpoint = [xMid,yMid]
+                        yMid = ySum / float(currentSize)
+                    midpoint = [xMid, yMid]
                     minDist = 1000
                     closestBlob = None
                     for blob in tempBlobs:
-                        dist = math.sqrt(math.pow(blob.midpoint[0], 2) + math.pow(blob.midpoint[1], 2))
+                        dist = math.sqrt(math.pow(blob.midpoint[0] - midpoint[0], 2) + math.pow(blob.midpoint[1] - midpoint[1], 2))
                         if dist < minDist:
                             minDist = dist
                             closestBlob = blob
-                    if (minDist < 40) and (minDist >= 0):
+                    if (minDist < 20) and (minDist >= 0):
                         minIndex = blobs.index(closestBlob)
                         blobs[minIndex].midpoint = midpoint
                         blobs[minIndex].blobSize = currentSize
@@ -118,80 +100,74 @@ def analyze():
         blobs.remove(blob)
 
 
-def fillUp(x,y):
-    global upCount, downCount, yMidSum, ySum, currentSize, r, g, b
-    y = y - 1
-    if(checkValid(x,y)):
-        pixdata[x,y] = (r, g, b)
+def fillUp(x, y):
+    global ySum, xSum, currentSize
+    y -= 1
+    if checkValid(x, y):
+        pixdata[x, y] = (0, 0, 0)
 
-        ySum+=1
-        upCount+=1
-        downCount -= 1
-        yMidSum -= upCount
+        ySum += y
+        xSum += x
         currentSize += 1
-        fillAll(x,y)
+        fillAll(x, y)
 
 
-def fillRight(x,y):
-    global rightCount, leftCount, xMidSum, xSum, currentSize, r, g, b
-    x = x + 1
-    if(checkValid(x,y)):
-        pixdata[x,y] = (r, g, b)
+def fillRight(x, y):
+    global ySum, xSum, currentSize
+    x += 1
+    if checkValid(x, y):
+        pixdata[x, y] = (0, 0, 0)
 
-        xSum += 1
-        rightCount += 1
-        leftCount -= 1
-        xMidSum += rightCount
+        ySum += y
+        xSum += x
         currentSize += 1
-        fillAll(x,y)
+        fillAll(x, y)
 
 
-def fillLeft(x,y):
-    global rightCount, leftCount, xMidSum, xSum, currentSize, r, g, b
-    x = x - 1
-    if(checkValid(x,y)):
-        pixdata[x,y] = (r, g, b)
+def fillLeft(x, y):
+    global ySum, xSum, currentSize
+    x -= 1
+    if checkValid(x, y):
+        pixdata[x, y] = (0, 0, 0)
 
-        xSum+=1
-        leftCount+=1
-        rightCount-=1
-        xMidSum-=leftCount
+        ySum += y
+        xSum += x
         currentSize += 1
-        fillAll(x,y)
+        fillAll(x, y)
 
 
-def fillDown(x,y):
-    global upCount, downCount, yMidSum, ySum, currentSize, r, g, b
-    y = y + 1
-    if(checkValid(x,y)):
-        pixdata[x,y] = (r, g, b)
+def fillDown(x, y):
+    global ySum, xSum, currentSize
+    y += 1
+    if checkValid(x, y):
+        pixdata[x, y] = (0, 0, 0)
 
-        ySum+=1
-        downCount+=1
-        upCount-=1
-        yMidSum+=downCount
+        ySum += y
+        xSum += x
         currentSize += 1
-        fillAll(x,y)
+        fillAll(x, y)
 
 
-def fillAll(x,y):
-    fillUp(x,y)
-    fillRight(x,y)
-    fillDown(x,y)
-    fillLeft(x,y)
+def fillAll(x, y):
+    fillUp(x, y)
+    fillRight(x, y)
+    fillDown(x, y)
+    fillLeft(x, y)
 
 
 def checkValid(x,y):
-    if(x<image.size[0]) and (y<image.size[1] and (x >= 0) and (y >= 0)):
-        if(pixdata[x, y][0]+pixdata[x, y][1]+pixdata[x, y][2]>400):
+    if(x < image.size[0]) and (y < image.size[1] and (x >= 0) and (y >= 0)):
+        if pixdata[x, y][0] + pixdata[x, y][1] + pixdata[x, y][2] > 350:
             return True
         else:
             return False
     else:
         return False
 
+
 def obj_dict(obj):
     return obj.__dict__
+
 
 def runCode():
     while True:
